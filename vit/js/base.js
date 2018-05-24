cce5ee04334c6979c20a55ed7a0d7e68d0cf57a8
ahezarkhani <armanhschool@gmail.com>
2018-05-24 17:42:20 -0400

    added press, navbar, roi calculator

--- vit/js/base.js
+++ vit/js/base.js
@@ -1,3 +1,5 @@
+var navShowing = true;
+
 function closeModal() {
     $(".modal-container").hide();
     $(".modal").hide();
@@ -8,8 +10,39 @@ function showModal() {
     $(".modal").show();
 }
 
+function hideNav() {
+    if($(".nav-icons-list").is(":visible") && !navShowing){
+        $(".nav-icons-list").hide();
+    }
+}
+
+function showNav() {
+    console.log("showNav")
+    if(! $(".nav-icons-list").is(":visible") && !navShowing){
+        $(".nav-icons-list").show();
+    }
+}
+
+function toggleNavHard() {
+    if($(".nav-icons-list").is(":visible")){
+        $(".nav-icons-list").hide();
+        navShowing = false;
+    } else {
+        $(".nav-icons-list").show();
+        navShowing = true;
+    }
+}
+
+function hideNavHard() {
+    $(".nav-icons-list").hide()
+}
+
+function showNavHard() {
+    $(".nav-icons-list").show()
+}
+
 
 window.onload = function() {
-    window.setTimeout(showModal, 5000)
+    window.setTimeout(showModal, 8000)
 
 };
--- vit/js/index.js
+++ vit/js/index.js
@@ -1,6 +1,19 @@
 var hiwSelectedItem = 0;
 setInterval(changeHowItWorksItem, 5000)
 
+var injuriesSlider = document.getElementById("injuries-per-year-range");
+var injuriesValue = document.getElementById("injuries-per-year-val");
+injuriesValue.innerHTML = injuriesSlider.value + " Strains and Sprains / year";
+var profitMarginSlider = document.getElementById("business-profit-margin-range");
+var profitMarginValue = document.getElementById("business-profit-margin-val");
+profitMarginValue.innerHTML = profitMarginSlider.value + "% Profit Margin";
+injuriesSlider.oninput = function() {
+    injuriesValue.innerHTML = this.value + " Strains and Sprains / year";
+}
+profitMarginSlider.oninput = function() {
+    profitMarginValue.innerHTML = this.value + "% Profit Margin";
+}
+
 
 function learnMoreScroll(){
     var info = document.getElementsByClassName("reasons")[0];
@@ -26,7 +39,48 @@ function changeHowItWorksItem() {
     children[hiwSelectedItem].id = "hiw-active"
 }
 
+function toggleROICalc() {
+    if($(".roi-calc").is(":visible")){
+        $(".roi-calc").hide()
+        $("#up-arrow").hide()
+        $("#roi-calc-button").css("box-shadow", "0 4px 8px 0 rgba(0,0,0,0.2)")
+    } else {
+        $(".roi-calc").show()
+        $("#up-arrow").show()
+        $("#roi-calc-button").css("box-shadow", "none")
+    }
+}
+
+
+function calculateROI(){
+    var numInjuries = injuriesSlider.value
+    var totalIndirectCosts = numInjuries * 35000;
+    var totalDirectCosts = numInjuries * 30000;
+    var totalCosts = totalDirectCosts + totalIndirectCosts;
+
+    var profitMargin = profitMarginSlider.value;
+    console.log(profitMargin)
+    var salesCoverInDirectCosts = Math.floor(totalIndirectCosts / (profitMargin / 100));
+    var salesCoverTotalCosts = Math.floor(totalCosts / (profitMargin / 100));
+
+    $("#directCost").html("$" + totalIndirectCosts.toLocaleString())
+    $("#indirectCost").html("$" + totalDirectCosts.toLocaleString())
+    $("#combinedCost").html("$" + totalCosts.toLocaleString())
+    $("#salesDirectCost").html("$" + salesCoverInDirectCosts.toLocaleString())
+    $("#salesIndirectCost").html("$" + salesCoverTotalCosts.toLocaleString())
+
+    $(".roi-results").show()
+}
 
 window.onload = function() {
     
 };
+
+
+
+
+
+
+
+
+
